9838e9f42a7df6ab734cb592857085129eef08c6
Salman <sallu123xx@gmail.com>
2025-11-18 09:33:25 +0530

    Tests on CI lesson's assignment, failed ci tests

---
+++ src/tests/auth.test.ts
@@ -0,0 +1,17 @@
+import { describe, expect, test } from "vitest";
+
+const person = {
+  isActive: true,
+  age: 32,
+};
+
+describe("person", () => {
+  test("person is defined", () => {
+    expect(person).toBeDefined();
+    expect(person).not.toBeDefined();
+  });
+
+  test("is active", () => {
+    expect(person.isActive).toBeTruthy();
+  });
+});
