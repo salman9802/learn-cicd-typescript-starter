4a333b0ded8b700872358b8eece4647762849cde
Salman <sallu123xx@gmail.com>
2025-11-18 13:51:35 +0530

    CI Security lesson's assignment, failing security check in CI

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@ import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import { defineConfig } from "eslint/config";
+import pluginSecurity from "eslint-plugin-security";
 
 export default defineConfig([
   {
@@ -12,4 +13,5 @@ export default defineConfig([
     ignores: ["coverage/*"],
   },
   tseslint.configs.recommended,
+  pluginSecurity.configs.recommended,
 ]);
